d6c0ae96c010c3915716edbfc06fc72c49a425d8
Seamus Mullan <seamusmullan2023@gmail.com>
2025-06-09 20:26:59 +0100

    remove bs css

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -1,5 +1,3 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 import '@mantine/core/styles.css'
 import { MantineProvider } from '@mantine/core'
 
@@ -9,36 +7,13 @@ import { shadcnCssVariableResolver } from './theme/cssVariableResolver'
 const theme = shadcnTheme
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <MantineProvider
       theme={theme}
       defaultColorScheme="dark"
       cssVariablesResolver={shadcnCssVariableResolver}
     >
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
+      <p>test</p>
     </MantineProvider>
   )
 }
